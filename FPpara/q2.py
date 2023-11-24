4ad0abc5d0980b43e3d86350c1f36a9b957107ca
nalaidou <125977285+nalaidou@users.noreply.github.com>
2023-11-24 11:47:16 +0800

    Add files via upload

---
+++ FPpara/q2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#q2评估预测能力
+def f(o,p,q):
+    o_mean = np.average(o)
+    a = q-o_mean
+    ss = np.sum(a**2)
+    b = p-q
+    press = np.sum(b**2)
+    f = 1 - press/ss
+    return f
