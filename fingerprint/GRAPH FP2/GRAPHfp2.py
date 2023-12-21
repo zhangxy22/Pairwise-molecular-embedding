9e3930dbc5eac5b73d0ab0db23ec71d723610d07
zxy <125977285+zhangxy22@users.noreply.github.com>
2023-12-21 18:17:30 +0800

    Update GRAPHfp2.py

--- fingerprint/GRAPH FP2/GRAPHfp2.py
+++ fingerprint/GRAPH FP2/GRAPHfp2.py
@@ -14,7 +14,7 @@ start_time = time.time()
 dataxd = pd.read_csv('gra_fd.csv')
 dataxa = pd.read_csv('gra_fa.csv')
 dataX = np.hstack((dataxd,dataxa))
-data = pd.read_csv('../../pce.csv')
+data = pd.read_csv('../pce.csv')
 dataY = data[['PCE']]
 print(dataX.shape, dataY.shape)
 
